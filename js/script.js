df346f2370a827ffbb6a568f43a1c0832c4ee1ff
Rizsan Zainal M <zm2407@gmail.com>
2025-01-15 00:36:30 +0700

    html 1 css 1 js 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const nameInput = document.getElementById('name');
+const namaYangdiinput = document.getElementById('namayangdiinput');
+
+namaYangdiinput.textContent = 'Anonim';
+
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+});
